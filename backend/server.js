925be15d52ad6fa962c2b731924b7c49b68b3f5e
Bayezid <mrbayezidhoshen@gmail.com>
2022-07-15 08:36:39 +0600

    middleware setup done for error handaling

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const chat = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -11,5 +12,7 @@ connectDB();
 app.use(express.json()); //to accept json data
 app.use("/", userRoutes);
 app.use("/", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(5000, console.log("Server Started with 5000 port"));
