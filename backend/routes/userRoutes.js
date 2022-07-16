11a1abc54629c61ea7d24a718cf2fd924d95acc0
Bayezid <mrbayezidhoshen@gmail.com>
2022-07-16 20:58:15 +0600

    Login Problem solve and login system totaly ok

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,6 @@ const loginFunc = require("../controller/loginControler");
 const router = express.Router();
 
 router.route("/signup").post(registerUser);
-router.route("/login").get(loginFunc);
+router.route("/login").post(loginFunc);
 
 module.exports = router;
